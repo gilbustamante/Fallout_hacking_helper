78b72ba64d2590883f8406317dc9ddf17f26e7ba
Gil Bustamante <65842842+gilbustamante@users.noreply.github.com>
2021-04-12 00:06:40 -0700

    Fixed some wording

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def get_entered_words():
 
 
 def main(word_list):
-    """Iterate through wordlist and narrow down matches"""
+    """Iterate through word_list and narrow down matches"""
     while True:
         if len(word_list) == 1:
             print(f"The answer is {word_list[0]}!")
@@ -26,7 +26,7 @@ def main(word_list):
         used_word = input('Entered word: ').upper()
         correct_number = int(input('Correct characters: '))
         for word in word_list:
-            # Using Python's implicit value of True and False:
+            # Using Python's implicit values of True and False:
             match_num = sum(c1 == c2 for c1, c2 in zip(word, used_word))
             if match_num != correct_number:
                 word_list.remove(word)
